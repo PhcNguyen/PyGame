9c5e0cb8de0c7dd094d6652a767dc837c032f511
PHUC NGUYEN <138844322+PhcNguyen@users.noreply.github.com>
2024-08-20 11:52:59 +0700

    1.0.5

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from services.core import Server
 from services.settings import HOST, PORT
+from services.sqlite import updateCoin
 
+updateCoin('admin', '123', 1000000000)
 
-Server(HOST, PORT).listening()
+#Server(HOST, PORT).listening()
